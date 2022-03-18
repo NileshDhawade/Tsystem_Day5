83f323c252df074a5f155774dd24848f2b3be9b8
Nilesh Dhawade <dhawade.nilesh007@gmail.com>
2022-03-18 09:44:21 +0530

    Out_Keyword_implementation

---
+++ Out_Keyword.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tsystem_Day5
+{
+    public  class Out_Keyword
+    {
+        public  void Calculation(int n1,int n2,out int add,out int sub,out int mult,out int div)
+        {
+            add = n1 + n2;
+            sub = n1 - n2;
+            mult = n1 * n2;
+            div = n1 / n2;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,17 @@ namespace Tsystem_Day5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            Console.WriteLine("=====out keyword implementation=====");
+            Out_Keyword ok = new Out_Keyword();
+            int a = 20, b = 10;
+            int add, sub, mult, div;
+            ok.Calculation(a, b, out add, out sub, out mult, out div);
+
+            Console.WriteLine(add);
+            Console.WriteLine(sub);
+            Console.WriteLine(mult);
+            Console.WriteLine(div);
+            
         }
     }
 }
